be0612e8eb8bcde9b828c00de005df3b40462b86
gchandrakanth89 <gchandrakanth89@gmail.com>
2016-05-28 12:23:28 +0530

    Closed the cursor to avoid resource leak.

--- app/src/main/java/com/gck/simplecontacts/ContactsListFragment.java
+++ app/src/main/java/com/gck/simplecontacts/ContactsListFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,8 +17,11 @@ import android.widget.ListView;
 public class ContactsListFragment extends Fragment {
 
 
+    private static final String TAG = "ContactsListFragment";
     private ListView listView;
     private ContactsAdapter contactsAdapter;
+    private Cursor searchCursor;
+    private Cursor allContactsCursor;
 
     public ContactsListFragment() {
     }
@@ -43,13 +47,24 @@ public class ContactsListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_contacts_list, container, false);
 
         listView = (ListView) view.findViewById(R.id.list_view);
-        contactsAdapter = new ContactsAdapter(getActivity(), Utils.getAllContactsCursor(getActivity()));
+        allContactsCursor = Utils.getAllContactsCursor(getActivity());
+        contactsAdapter = new ContactsAdapter(getActivity(), allContactsCursor);
         listView.setAdapter(contactsAdapter);
 
         EditText editText = (EditText) view.findViewById(R.id.edit_text);
         editText.addTextChangedListener(textWatcher);
 
         return view;
+    }
+
+    @Override
+    public void onDetach() {
+        allContactsCursor.close();
+        if(searchCursor!=null){
+            Log.d(TAG,"Closing cursor");
+            searchCursor.close();
+        }
+        super.onDetach();
     }
 
     private TextWatcher textWatcher = new TextWatcher() {
@@ -60,7 +75,6 @@ public class ContactsListFragment extends Fragment {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            Cursor searchCursor;
             if (TextUtils.isEmpty(s)) {
                 searchCursor = Utils.getAllContactsCursor(getActivity());
             } else {
