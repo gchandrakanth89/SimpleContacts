45b57c8eacefc8912a1882e7a27e6bf61a5a04a5
gchandrakanth89 <gchandrakanth89@gmail.com>
2016-05-30 11:40:10 +0530

    ContactDetailsFragment added

---
+++ app/src/main/java/com/gck/simplecontacts/ContactDetailsFragment.java
@@ -0,0 +1,42 @@
+package com.gck.simplecontacts;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ContactDetailsFragment extends Fragment {
+    private static final String KEY_CONTACT_ID = "param1";
+
+    private int contactId;
+
+
+    public ContactDetailsFragment() {
+    }
+
+    public static ContactDetailsFragment newInstance(int contactId) {
+        ContactDetailsFragment fragment = new ContactDetailsFragment();
+        Bundle args = new Bundle();
+        args.putInt(KEY_CONTACT_ID, contactId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            contactId = getArguments().getInt(KEY_CONTACT_ID);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contact_details, container, false);
+    }
+
+}
